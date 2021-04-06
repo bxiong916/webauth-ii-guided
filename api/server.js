8dfbf6049c5e2bdea6d09c7b2bf61a59c029ecb4
Bill Xiong <xiong.bill@yahoo.com>
2021-04-05 22:22:20 -0700

    progress

--- api/server.js
+++ api/server.js
@@ -1,21 +1,35 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
+
+const sessionConfig = {
+  name: "monkey", // sid,
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 1000 * 30,
+    secure: false, // true in production,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: true, // GDPR laws against setting cookies automatically (always false!!)
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+server.use(session(sessionConfig));
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
 
 module.exports = server;
